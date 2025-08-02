4f1fad4653f0033fd80010599272f144f017432e
pateldhruv17 <dhruvpatel13.dp@gmail.com>
2025-08-03 01:53:16 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center bg-gray-100 text-center">
       <div>
         <h1 className="text-4xl font-bold">Next.js Demo Project</h1>
-        <h1 className="text-5xl font-bold text-center text-blue-500">
-          This is my new version for the Preview!!!
+        <h1 className="text-5xl font-bold text-center text-green-500">
+          This is my main new version for the Preview!!!
         </h1>
       </div>
     </main>
